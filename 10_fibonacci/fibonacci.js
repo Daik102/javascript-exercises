f2ee4abec76808305926e0f401fb1d11297c8e50
Daik <fruit@kd6.so-net.ne.jp>
2025-06-02 14:15:37 +0900

    Finish exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  let result = 0;
+  let fMinusTwo = 0;
+  let fMinusOne = 1;
+  const num = Number(number);
 
-};
+  if (num < 0) return 'OOPS';
+  if (num === 1) return fMinusOne;
+  
+  for (let i = 2; i <= num; i++) {
+    result = fMinusTwo + fMinusOne;
+    fMinusTwo = fMinusOne;
+    fMinusOne = result;
+  }
+   return result; 
+  }
 
 // Do not edit below this line
 module.exports = fibonacci;
