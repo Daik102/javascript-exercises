2858984d2722142ae3bf973edd45462dcf2499c9
Daik <fruit@kd6.so-net.ne.jp>
2025-06-02 12:18:27 +0900

    Finish exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const char = /[a-z1-9]/g;
+  const matched = string.toLowerCase().match(char);
+  
+  if (matched.join('') === matched.reverse().join('')) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
